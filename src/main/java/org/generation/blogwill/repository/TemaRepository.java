324022489021c6f9a9c0124296ca59f44298f5de
Will Matheus <119905167+OWillMatheuz@users.noreply.github.com>
2023-01-27 14:34:22 -0300

    Update TemaRepository.java

--- src/main/java/org/generation/blogwill/repository/TemaRepository.java
+++ src/main/java/org/generation/blogwill/repository/TemaRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.generation.blogwill.model.Tema;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long>{
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
 		
