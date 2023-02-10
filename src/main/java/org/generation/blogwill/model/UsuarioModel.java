443da90f8604c5ce95c12745f1b23ebd60d7a225
Will Matheus <dev.owillmatheus@gmail.com>
2023-02-10 12:07:49 -0300

    Deploy do projeto Blog Pessoal

--- src/main/java/org/generation/blogwill/model/UsuarioModel.java
+++ src/main/java/org/generation/blogwill/model/UsuarioModel.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name= "tb_usuarios")
 public class UsuarioModel {
@@ -26,6 +28,7 @@ public class UsuarioModel {
 	@NotNull(message = "O atributo Nome é Obrigatório! ")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo Usuário é Obrigatório! ")
 	@Email(message = "O atributo Usuário deve ser um email válido! ")
 	private String usuario;
