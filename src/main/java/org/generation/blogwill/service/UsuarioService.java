b004ac2141274cf24f96fd4e3e5394d867dc68f4
Will Matheus <119905167+OWillMatheuz@users.noreply.github.com>
2023-03-07 20:18:24 -0300

    correção da classe service

--- src/main/java/org/generation/blogwill/service/UsuarioService.java
+++ src/main/java/org/generation/blogwill/service/UsuarioService.java
@@ -90,9 +90,9 @@ public class UsuarioService {
 
 	    private String gerarBasicToken(String usuario, String senha) {
 
-	        String token = usuario + ":" + senha;
+	        String token = usuario + " : " + senha;
 	        byte[] tokenBase64 = Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII")));
-	        return "Basic" + new String(tokenBase64);
+	        return "Basic " + new String(tokenBase64);
 
 	    }
 }
